007dea97106fc98b3fa5eb81e129f160ef4e89b4
slaur88 <laurentsteve8@gmail.com>
2026-03-13 13:48:23 +0100

    ajout JWT

---
+++ EventAssos.Core/DTOs/Responses/LoginResponseDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventAssos.Core.DTOs.Responses;
+
+public class LoginResponseDto
+{
+    public string Token { get; set; } = null!;
+    public DateTime Expiration { get; set; }
+}
