a77092f08b775dbb22d1461c74c0981132dfd0c9
slaur88 <laurentsteve8@gmail.com>
2026-03-16 09:38:02 +0100

    JWT Scalar

---
+++ EventAssos.API/Scalar/BearerSecuritySchemeTransformer.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.OpenApi;
+using Microsoft.OpenApi;
+
+namespace EventAssos.API.Scalar;
+
+internal sealed class BearerSecuritySchemeTransformer :IOpenApiDocumentTransformer
+{
+    private readonly IAuthenticationSchemeProvider _authenticationSchemeProvider;
+
+    public BearerSecuritySchemeTransformer(IAuthenticationSchemeProvider authenticationSchemeProvider)
+    {
+        _authenticationSchemeProvider = authenticationSchemeProvider;
+    }
+
+    public async Task TransformAsync(OpenApiDocument document, OpenApiDocumentTransformerContext context, CancellationToken cancellationToken)
+    {
+        var authenticationSchemes = await _authenticationSchemeProvider.GetAllSchemesAsync();
+        if (!authenticationSchemes.Any(a => a.Name == "bearer"))
+            return;
+
+        var bearerScheme = new OpenApiSecurityScheme
+        {
+            Type = SecuritySchemeType.Http,
+            Scheme = "bearer",
+            BearerFormat = "JWT",
+            In = ParameterLocation.Header,
+            Description = "JWT Authorization header using the Bearer scheme."
+        };
+
+        document.Components ??= new OpenApiComponents();
+
+        document.AddComponent("bearer", bearerScheme);
+
+        var securityRequirement = new OpenApiSecurityRequirement
+        {
+            [new OpenApiSecuritySchemeReference("bearer", document)] = []
+        };
+
+        foreach (var path in document.Paths.Values)
+        {
+            foreach (var operation in path.Operations.Values)
+            {
+                operation.Security ??= new List<OpenApiSecurityRequirement>();
+                operation.Security.Add(securityRequirement);
+            }
+        }
+    }
+}
