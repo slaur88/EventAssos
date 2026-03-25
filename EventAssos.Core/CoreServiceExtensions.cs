c68c65f1f76a9d54cd6e3c80690df617eded4ff5
f.russo <f.russo@GOS-VDI307>
2026-03-25 11:00:27 +0100

    Email sender terminé

---
+++ EventAssos.Core/CoreServiceExtensions.cs
@@ -0,0 +1,35 @@
+﻿using EventAssos.Core.Interfaces.Services.Auth;
+using EventAssos.Core.Interfaces.Services.Tools;
+using EventAssos.Core.Services.Auth;
+using EventAssos.Core.Services.Tools;
+using EventAssos.Core.Settings;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventAssos.Core
+{
+    public static class CoreServiceExtensions
+    {
+        public static IServiceCollection AddCoreServices(
+        this IServiceCollection services,
+        IConfiguration configuration)
+        {
+            // Settings email
+            var emailSettings = configuration
+                .GetSection("EmailSettings")
+                .Get<EmailSettings>();
+            services.AddSingleton(emailSettings);
+
+            // Services 
+            services.AddScoped<IAuthService, AuthService>();
+            services.AddScoped<IEmailService, EmailService>();
+            services.AddScoped<IPasswordHasherService, PasswordHasherService>();
+            services.AddScoped<IPasswordGeneratorService, PasswordGeneratorService>();
+
+            return services;
+        }
+    }
+}
---
+++ EventAssos.Core/Settings/EmailSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventAssos.Core.Settings
+{
+    public class EmailSettings
+    {
+        public string Host { get; set; } = null!;
+        public int Port { get; set; }
+        public string SenderEmail { get; set; } = null!;
+        public string SenderName { get; set; } = null!;
+        public string Password { get; set; } = null!;
+    }
+}
