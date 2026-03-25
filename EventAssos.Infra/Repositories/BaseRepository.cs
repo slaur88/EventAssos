77721d7ceeba0ffcaac3272cf9f9cb386c7c69cd
f.russo <f.russo@GOS-VDI307>
2026-03-25 14:27:09 +0100

    Création Context et imple CRUD

---
+++ EventAssos.Infra/Repositories/BaseRepository.cs
@@ -0,0 +1,58 @@
+﻿using EventAssos.Core.Interfaces.Repositories;
+using EventAssos.Infra.Database.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace EventAssos.Infra.Repositories
+{
+    public class BaseRepository<TEntity, TKey>(EventAssosContext _context)
+    : IBaseRepository<TEntity, TKey> 
+    where TEntity : class
+    where TKey : struct
+
+    {
+        protected readonly DbSet<TEntity> _entities = _context.Set<TEntity>();
+
+    
+        public async Task<TEntity> AddAsync(TEntity entity)
+        {
+            await _entities.AddAsync(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task DeleteAsync(TKey id)
+        {
+            var entity = await _entities.FindAsync(id);
+            if (entity != null)
+            {
+                _entities.Remove(entity);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public async Task<IEnumerable<TEntity>> FindAsync(Expression<Func<TEntity, bool>> predicate)
+        {
+            return await _entities.Where(predicate).ToListAsync();
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync()
+        {
+            return await _entities.ToListAsync();
+        }
+
+        public async Task<TEntity?> GetByIdAsync(TKey id)
+        {
+            return await _entities.FindAsync(id);
+        }
+
+        public async Task UpdateAsync(TKey id, TEntity entity)
+        {
+            _entities.Update(entity);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
