eb460901d8fe371b38e8910cd12deb16a5ac2095
f.russo <f.russo@GOS-VDI307>
2026-03-26 10:17:04 +0100

    Ajout de AuthController, Login, InfraServiceExtension, ConnectionString, LoginRequestDTO

---
+++ EventAssos.API/Controllers/AuthController.cs
@@ -0,0 +1,49 @@
+﻿using EventAssos.Core.DTOs.Requests;
+using EventAssos.Core.DTOs.Responses;
+using EventAssos.Core.Interfaces.Services.Auth;
+using EventAssos.Core.Services.Auth;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace EventAssos.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController(IAuthService _authService) : ControllerBase
+    {
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(RegisterRequestDTO request)
+        {
+            try
+            {
+                var createdUser = await _authService.RegisterAsync(request);
+
+                return CreatedAtAction(
+                    actionName: "GetUser",
+                    controllerName: "Users",
+                    routeValues: new { id = createdUser.Id },
+                    value: createdUser
+                    );
+            }
+            catch (InvalidOperationException ex)
+            {
+                return Conflict(new { ex.Message });
+            }
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<LoginResponseDto>> Login([FromBody] LoginRequestDTO request)
+        {
+            try
+            {
+                var loginResponse = await _authService.Login(request);
+                return Ok(loginResponse);
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                return Unauthorized(new { ex.Message });
+            }
+        }
+    }
+}
---
+++ EventAssos.Infra/InfraServiceExtensions.cs
@@ -0,0 +1,25 @@
+﻿using EventAssos.Core.Interfaces.Repositories;
+using EventAssos.Infra.Database.Context;
+using EventAssos.Infra.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventAssos.Infra
+{
+    public static class InfraServiceExtensions
+    {
+        public static void AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            // Ajouter toutes les configurations liées à l'Infrastructure (ex: DbContext, Repositories, etc.)
+            var connectionString = configuration.GetConnectionString("Default");
+            services.AddDbContext<EventAssosContext>(options => options.UseSqlServer(connectionString));
+
+            services.AddScoped<IUserRepository, UserRepository>();
+
+        }
+    }
+}
