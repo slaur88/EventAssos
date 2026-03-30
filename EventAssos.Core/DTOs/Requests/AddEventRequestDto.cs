04f052d1c84ec0b40215803148d217069c53f8f4
slaur88 <laurentsteve8@gmail.com>
2026-03-30 11:19:05 +0200

    modif add event

--- EventAssos.Core/DTOs/Requests/AddEventRequestDto.cs
+++ EventAssos.Core/DTOs/Requests/AddEventRequestDto.cs
@@ -10,14 +10,10 @@ namespace EventAssos.Core.DTOs.Requests;
 public class AddEventRequestDto
 {
     [Required(ErrorMessage = "Le titre de l'événement est requis.")]
-    public string Title { get; set; } =null!;
+    public string Name { get; set; } =null!;
     [Required(ErrorMessage = "La description de l'événement est requise.")]
         public string Description { get; set; } = null!;
 
-    public DateTime? StartDate { get; set; }
-    public DateTime? EndDate { get; set; }
-
-    public string? lieu { get; set; }
     [Required] 
     [Range(1,200, ErrorMessage = "Le nombre minimum de participants doit être supérieur ou égal à 1.")]
     public int NbMin { get; set; }
@@ -26,9 +22,7 @@ public class AddEventRequestDto
     public int NbMax { get; set; }
     [Required(ErrorMessage = "Le statut de l'événement est requis.")]
     public EventStatut Statut { get; set; }
-    public DateOnly CreationDate { get; set; }
-    [Required(ErrorMessage = "La date limite d'inscription est requise.")]
-    public DateTime LimiteInscription { get; set; }
+
 
 
 }
